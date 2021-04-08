6afc02c54097638d8e8f6becd2a9f85cc4fc1326
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-04-08 10:03:37 -0400

    switch to MongoDB Atlas

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,7 +1,4 @@
-export const URI = "mongodb://localhost/webd6201";
+export const LocalURI = "mongodb://localhost/webd6201";
+export const RemoteURI = "mongodb+srv://thomas:5wWyrxbVa59twJoh@webd6201.zvqij.mongodb.net/webd6201?retryWrites=true&w=majority";
 export const Secret = "someSecret";
-
-/* module.exports = {
-    Path: mongoDBPath,
-    Secret: sessionSecret
-}  */
+export const Host = "MongoDB Atlas";
